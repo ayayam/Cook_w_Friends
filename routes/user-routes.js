06ea1febe9c8e93c0d5a8408ed6a93ac38fea920
ayayam <aya.s.yamamoto@gmail.com>
2023-06-05 20:44:50 -0400

    Revert "Revert "pushing for debugging user-controllers""
    This reverts commit cdd05bd8ddf265430095d8462ff351a0dab3392e.
    
    :wq

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -9,8 +9,9 @@ router.route('/:_id')
 router.route('/:_id/profile-edit')
     .get(userControllers.profile_edit_get)
 
-
 // user/profile/:_id/cookbook/:_id
+
+//taking out :_id from cookbook route to see if that's what's getting in the way of passing Recipes model data *DIDN'T WORK*
 router.route('/:_id/cookbook')
     .get(userControllers.cookbook_get)
     .post(userControllers.create_recipe_post)
